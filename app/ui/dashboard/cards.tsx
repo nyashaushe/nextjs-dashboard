96626268ba051d83c50ecb52bc50ec9d49ba0032
Nyasha Shepard Ushewokunze <mrshepard18@gmail.com>
2025-05-31 10:07:58 +0200

    card update

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -48,13 +48,12 @@ export function Card({
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
-      <div className="flex p-4">
+      <div className="flex items-center p-4">
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
-        className={`${lusitana.className}
-          truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
+        className={`${lusitana.className} truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
       >
         {value}
       </p>
